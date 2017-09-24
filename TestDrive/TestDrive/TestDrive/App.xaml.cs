7e60342a47b1ab76ff5b571cb0466c736e6ed2b6
Danilo Oliveira <danilotecoliveira@yahoo.com.br>
2017-09-24 20:25:27 -0300

    Login View and validation

--- TestDrive/TestDrive/TestDrive/App.xaml.cs
+++ TestDrive/TestDrive/TestDrive/App.xaml.cs
@@ -1,4 +1,6 @@
-﻿using Xamarin.Forms;
+﻿using TestDrive.Models;
+using TestDrive.Views;
+using Xamarin.Forms;
 
 namespace TestDrive
 {
@@ -8,12 +10,15 @@ namespace TestDrive
         {
             InitializeComponent();
 
-            MainPage = new NavigationPage(new Views.ListagemView());
+            MainPage = new LoginView();
         }
 
         protected override void OnStart()
         {
-            // Handle when your app starts
+            MessagingCenter.Subscribe<Usuario>(this, "SucessoLogin", (usuario) => 
+            {
+                MainPage = new NavigationPage(new ListagemView());
+            });
         }
 
         protected override void OnSleep()
---
+++ TestDrive/TestDrive/TestDrive/ViewModels/LoginViewModel.cs
@@ -0,0 +1,44 @@
+﻿using Xamarin.Forms;
+using TestDrive.Models;
+using System.Windows.Input;
+
+namespace TestDrive.ViewModels
+{
+    public class LoginViewModel : BaseViewModel
+    {
+        private string usuario;
+        public string Usuario
+        {
+            get { return usuario; }
+            set {
+                usuario = value;
+                ((Command)EntrarCommand).ChangeCanExecute();
+            }
+        }
+
+        private string senha;
+        public string Senha
+        {
+            get { return senha; }
+            set {
+                senha = value;
+                ((Command)EntrarCommand).ChangeCanExecute();
+            }
+        }
+
+        public ICommand EntrarCommand { get; private set; }
+
+        public LoginViewModel()
+        {
+            EntrarCommand = new Command(() =>
+            {
+                MessagingCenter.Send(new Usuario(), "SucessoLogin");
+            },
+                () => 
+                {
+                    return (!string.IsNullOrWhiteSpace(usuario) && !string.IsNullOrWhiteSpace(senha));
+                }
+            );
+        }
+    }
+}
